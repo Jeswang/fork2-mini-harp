0c37f5877fe26bb2bb0dfe8d49c3ac77c4e94ff5
jeswang <wangyi724@gmail.com>
2014-05-05 21:09:14 +0800

    Implement A Current Time Middleware

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 
 function createMiniHarp(){
   var connect = require('connect');
-  var app = connect();
+  var app = connect().use(
+      function(req, res){
+        if(req.url == '/current-time'){
+          res.end((new Date()).toISOString());
+        }
+        else{
+          res.end('Cannot Get '+req.url);
+        }
+      });
   return app;
 }
 
